5ee9101fba2c9a0d6c064931f88eff45663623a7
Liza Volyanskaya <you@example.com>
2023-08-16 19:16:16 +0300

    commit12

--- js/my.js
+++ js/my.js
@@ -2,17 +2,23 @@ const button = document.querySelector('.title')
 let div = document.createElement('div')
 div.className= 'alert'
 div.innerHTML = 'Концентрація альбуміну'
-
+let numberOfSamples
+let sum
 button.addEventListener('touchstart', createBox)
 
 function createBox(){
     document.body.append(div)
+     numberOfSamples = prompt("Введіть число проб плазми")
+     div.innerHTML = 'gggg'
 }
 
-button.addEventListener('click', createBox)
+button.addEventListener('click',  createBox)
 
 function createBox(){
     document.body.append(div)
+    numberOfSamples = prompt("Введіть оптичну щільність зразка")
+    sum = (numberOfSamples / 1.028) * 50 
+    div.innerHTML = `Концентрація альбуміну: ${sum} г/л`
 }
 
 
